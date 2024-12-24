1fc611c56aeeb5e794d68fc7e0a67773099e137a
Gaurav Ahuja <gaurav.ahuja1991@gmail.com>
2024-12-24 11:35:21 +0000

    maven testNG test running

---
+++ src/test/java/JavaHelloWorldTestNgTC_Test.java
@@ -0,0 +1,9 @@
+import static org.testng.Assert.*;
+
+public class JavaHelloWorldTestNgTC_Test {
+
+    @org.testng.annotations.Test
+    public void testGetHello() {
+        System.out.println("hello from testng test");
+    }
+}
--- src/test/java/JavaHelloWorldTest_JunitTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-
-public class JavaHelloWorldTest_JunitTest {
-
-    @org.junit.Test
-    public void getHello() {
-        System.out.println("test from junit");
-    }
-}
