68bbd32ca134483bcb7bd4038fd76e282f9b50e8
Gaurav Ahuja <gaurav.ahuja1991@gmail.com>
2024-12-24 11:55:31 +0000

    maven reporting surefire plugin

---
+++ src/test/java/JavaHelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        System.out.println("test from junit 5");
+    }
+}
--- src/test/java/JavaHelloWorldTestNgTC_Test.java
+++
@@ -1,9 +0,0 @@
-import static org.testng.Assert.*;
-
-public class JavaHelloWorldTestNgTC_Test {
-
-    @org.testng.annotations.Test
-    public void testGetHello() {
-        System.out.println("hello from testng test");
-    }
-}
